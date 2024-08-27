0e0ff3f689013d15f2ccb64931431cd07c838be4
Karima [ Matrix ] <123845298+karima-mahmoud@users.noreply.github.com>
2024-08-28 02:11:26 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,13 +8,13 @@ with open('model3.pkl', 'rb') as file:
 st.title("diabetes patient")
 
 Pregnancies = st.sidebar.number_input('Pregnancies' , min_value=0.0 , max_value=20.0,value=1.0)
-Glucose = st.sidebar.number_input('Glucose' ,min_value= 0.0 ,max_value=200.0,value=100.0)
-blood_pressure =st.sidebar.number_input('blood_pressure' ,min_value=0.0 ,max_value= 130.0,value=70.0)
-skin_thickness =st.sidebar.number_input('skin_thickness' , min_value=0.0 , max_value=100.0,value=20.0)
-Insulin =  st.sidebar.number_input('Insulin' , min_value=0.0 , max_value=900.0,value=30.0)
-BMI =  st.sidebar.number_input('BMI' , min_value=0.0 , max_value=70.0,value=15.0)
+Glucose = st.sidebar.number_input('Glucose' ,min_value= 0.0 ,max_value=200.0,value=1.0)
+blood_pressure =st.sidebar.number_input('blood_pressure' ,min_value=0.0 ,max_value= 130.0,value=1.0)
+skin_thickness =st.sidebar.number_input('skin_thickness' , min_value=0.0 , max_value=100.0,value=1.0)
+Insulin =  st.sidebar.number_input('Insulin' , min_value=0.0 , max_value=900.0,value=1.0)
+BMI =  st.sidebar.number_input('BMI' , min_value=0.0 , max_value=70.0,value=1.0)
 DiabetesPedigreeFunction =  st.sidebar.number_input('DiabetesPedigreeFunction' , min_value=0.0 , max_value=2.5,value=0.5)
-Age =  st.sidebar.number_input('Age' , min_value=15.0 , max_value=100.0,value=25.0)
+Age =  st.sidebar.number_input('Age' , min_value=15.0 , max_value=100.0,value=1.0)
 
 output = model.predict([[Pregnancies,Glucose,blood_pressure,skin_thickness,Insulin,BMI,DiabetesPedigreeFunction,Age]])
 prediction_proba = model.predict_proba([[Pregnancies,Glucose,blood_pressure,skin_thickness,Insulin,BMI,DiabetesPedigreeFunction,Age]])[0]
