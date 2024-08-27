4df8bbc0666c98e1faeb17c2a115cb9711bffc07
Karima [ Matrix ] <123845298+karima-mahmoud@users.noreply.github.com>
2024-08-27 21:17:49 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,16 @@
 import streamlit as st
 import pickle
 
-
-# load pkl file
 with open('model.pkl', 'rb') as file:
     model = pickle.load(file)
 
-#title the page
+
 st.title("diabetes patient")
 
-#set image
-
-#inputs
 Pregnancies = st.number_input('Pregnancies' , min_value=0.0 , max_value=10.0,value=1.0)
 Glucose = st.number_input('Glucose' , min_value=0.0 , max_value=10.0,value=1.0)
+blood_pressure =st.number_input('blood_pressure' , min_value=0.0 , max_value=100.0,value=1.0)
+skin_thickness =st.number_input('skin_thickness' , min_value=0.0 , max_value=100.0,value=1.0)
 Insulin =  st.number_input('Insulin' , min_value=0.0 , max_value=100.0,value=1.0)
 BMI =  st.number_input('BMI' , min_value=0.0 , max_value=100.0,value=1.0)
 DiabetesPedigreeFunction =  st.number_input('DiabetesPedigreeFunction' , min_value=0.0 , max_value=100.0,value=1.0)
@@ -21,5 +18,4 @@ Age =  st.number_input('Age' , min_value=0.0 , max_value=100.0,value=1.0)
 
 output = model.predict([[Pregnancies,Glucose,Insulin,BMI,DiabetesPedigreeFunction,Age]])
 
-#display the result
 st.write("diabetes patient : ",round(output[0],2))
